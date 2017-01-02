6c6271937c795dc30922c09533222203c01ca041
Rupehra Chouhan <rupehra1@gmail.com>
2017-01-02 16:20:34 -0700

    ADD: 374 - Accepted

---
+++ 403/Main374.java
@@ -0,0 +1,28 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main374 {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger b, p,m;
+        ArrayList<BigInteger> answers = new ArrayList<BigInteger>();
+        while (sc.hasNext()) {
+            b = sc.nextBigInteger();
+            p = sc.nextBigInteger();
+            m = sc.nextBigInteger();
+            answers.add(calculateModPow(b,p,m));
+        }
+       printAnswers(answers);
+    }
+    private static BigInteger calculateModPow(BigInteger b, BigInteger p, BigInteger m) {
+        BigInteger answer = b.modPow(p,m);
+        return answer;
+    }
+    private static void printAnswers(ArrayList<BigInteger> answers) {
+        for(BigInteger b : answers) {
+            System.out.println(b);
+        }
+    }
+
+}
