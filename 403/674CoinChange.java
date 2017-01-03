e44f1eb41d6a21cce301a9210f92fda7a22115e5
Rupehra Chouhan <rupehra1@gmail.com>
2017-01-03 12:48:49 -0700

    MOd: 674 - Accepted

--- 403/674CoinChange.java
+++ 403/674CoinChange.java
@@ -2,32 +2,33 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class 674CoinChange {
-    public static void main(String []args) {
-        Scanner sc = new Scanner((System.in));
-        ArrayList<Integer> input = new ArrayList<Integer>();
-        while(sc.hasNext())
-            input.add(sc.nextInt());
-        for (int i = 0; i < input.size(); i++) {
-            System.out.println(numberOfWays(input.get(i)));
-        }
-    }
+  public static void main(String []args) {
+      Scanner sc = new Scanner((System.in));
+      ArrayList<Integer> input = new ArrayList<Integer>();
+      while(sc.hasNext())
+          input.add(sc.nextInt());
+      int[][] arr = numberOfWays();
+      for (int i = 0; i < input.size(); i++) {
+          System.out.println(arr[4][input.get(i)]);
+      }
+  }
 
-    private static Integer numberOfWays(Integer total) {
-        int[][] arr = new int[5][total+1];
-        for (int i = 0; i <total+1 ; i++)
-            arr[0][i] = 1;
-        for (int i = 0; i <5 ; i++)
-            arr[i][0] = 1;
-        int[] coins = {1,5,10,25,50};
-        for(int i=1; i<5; i++) {
-            int coin = coins[i];
-            for(int j=1; j<total+1; j++) {
-                if(j<coin)
-                    arr[i][j] = arr[i-1][j];
-                else
-                    arr[i][j] = arr[i-1][j] + arr[i][j-coin];
-            }
-        }
-        return arr[4][total];
-    }
+  private static int[][] numberOfWays() {
+      int[][] arr = new int[5][7490];
+      for (int i = 0; i <7490 ; i++)
+          arr[0][i] = 1;
+      for (int i = 0; i <5 ; i++)
+          arr[i][0] = 1;
+      int[] coins = {1,5,10,25,50};
+      for(int i=1; i<5; i++) {
+          int coin = coins[i];
+          for(int j=1; j<7490; j++) {
+              if(j<coin)
+                  arr[i][j] = arr[i-1][j];
+              else
+                  arr[i][j] = arr[i-1][j] + arr[i][j-coin];
+          }
+      }
+      return arr;
+  }
 }
